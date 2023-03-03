2a0f83348b37c8346a097205d2fe1f037dff653f
Nadiawardatunnisa <126864685+Nadiawardatunnisa@users.noreply.github.com>
2023-03-03 17:20:26 +0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+      <div class="login">
+
+          <div class="avatar">
+            <i class="fa fa-user"></i>
+          </div>
+
+          <h2>Login Siswa</h2>
+
+          <div class="box-login">
+            <i class="fas fa-lock"></i>
+            <input type="text" placeholder="Nisn">
+          </div>
+
+          <div class="box-login">
+            <i class="fas fa-lock"></i>
+            <input type="text" placeholder="Nis">
+          </div>
+
+          <button type="submit" name="login" class="btn-login">Login</button>
+         <a href="index.php" class="a">Login Sebagai Administrator / Petugas </a>
+          </div>
+      </div>
+  </head>
+  </html>
